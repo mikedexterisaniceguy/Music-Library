04c61cc065678ff70dec236587a3f2ab181cb451
Kiryl Maksimau <mikedexterisaniceguy@gmail.com>
2022-10-21 10:49:17 +0300

    Deleted the storyboard, added SceneDelegate changes

---
+++ Music Library/Music Library/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Library
+//
+//  Created by 123 on 20.10.22.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
